fb02153462cb023d3bbc5a77350f07637ee8a741
zhuang002 <zhuang001@gmail.com>
2024-05-20 12:00:41 -0400

    using arraylist, possibly slower

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
-import java.util.HashSet;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		HashSet<Integer> set = new HashSet<>();
+		ArrayList<Integer> al = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		for (int i=0;i<n;i++) 
-			set.add(sc.nextInt());
-		System.out.println(set.size());
+		for (int i=0;i<n;i++) {
+			int ele = sc.nextInt();
+			if (!al.contains(ele))
+				al.add(ele);
+		}
+			
+		System.out.println(al.size());
 	}
 
 }
